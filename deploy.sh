8d0dc12a21f4b5bae77215814a78f9117ed79658
Geoffrey Tipton <tipton@mysecretroom.com>
2018-03-27 11:00:55 +0800

    Update Back image and deployment Script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,42 @@
 #!/bin/bash
-echo "Identify the target server"
-ssh -p 22 $2 "sudo /home/ubuntu/role.sh"
+echo "--=== Incoming Paramters (This script hould be reusable) ===--"
+echo "[P1] Version Number is :$1 "
+echo "[P2] Target Server is :$2 "
+echo "[P3] Target Folder is :$3 "
+echo "---------------------------------------"
 
-echo "Version to Deploy is $1"
+echo "--=== Identify the taret server ===--"
+ssh -p 22 $2 "sudo /home/ubuntu/role.sh"
+ssh -p 22 $2 "sudo whoami"
+echo "---------------------------------------"
+
+echo "--=== Run local Tests on Deployment ===--"
+echo "No tests yet Defined"
+echo "-----------------------------------------"
+
+echo "--=== Modify Version Information ===--"
+echo "Version $1" > version.html
+echo "--------------------------------------"
+
+echo "--=== Transfer files to remote Server ===--"
+ls  -l
+rsync -avzhe ssh  --rsync-path="sudo rsync" . jenkins@$2:$3
+# /home/apache/public_html/wallet.aencoin.com/
+echo "---------------------------------------"
+
+echo "--=== Set permisions on transfered files ===--"
+
+echo "----------------------------------------------"
+
+echo "--=== Operations on the Remote server ===--"
+ssh -p 22 $2 "sudo chown -R apache:apache $3"
+echo "-------------------------------------------"
+
+echo "----====== Verify Deployments-List from Remote ======----"
+ssh -p 22 $2 "ls -al /home/apache/public_html/"
+ssh -p 22 $2 "ls -al $3"
+echo "---------------------------------------------------------"
+
+echo "--=== Version Deployed is [$1] The folowing output from version.info ===--"
+ssh -p 22 $2 "cat $3/version.info"
+echo "------------The-End-------------------------------------------------------"
