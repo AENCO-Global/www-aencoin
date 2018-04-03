f5461f99bb1fc4bf537282ddf1ebca252acbe52c
Geoffrey Tipton <tipton@mysecretroom.com>
2018-04-03 17:47:37 +0800

    team format

---
+++ www/team_files/2qCFheZccsI.js
@@ -0,0 +1,3 @@
+if (self.CavalryLogger) { CavalryLogger.start_js(["MdWRT"]); }
+
+__d("AdsLoggerConstants",[],(function a(b,c,d,e,f,g){f.exports={DAO_ERROR_EVENT_NAME:"dao_error",ERROR_EVENT_CATEGORY:"errors",UNHANDLED_JS_EXCEPTION_EVENT_NAME:"unhandled_js_exception"}}),null);
