5be7495f4db7a3514e0f3af64e8713e7430659c1
Isaac <issac.cheung@aencoin.com>
2018-09-06 11:56:11 +0800

    Add coinmake in trading platform and re-ordering

--- app/src/partners.js
+++ app/src/partners.js
@@ -6,12 +6,12 @@ module.exports = [
         url: "https://www.idax.mn"
     },
     {
-        image: "./assets/img/partners/coinmake.png",
-        url: "https://www.coinmake.com/"
-    },
-    {
         image: "./assets/img/partners/latoken.png",
         url: "https://latoken.com/"
+    },
+    {
+        image: "./assets/img/partners/coinmake.png",
+        url: "https://www.coinmake.com/"
     },
     {
         image: "./assets/img/partners/eea.png",
